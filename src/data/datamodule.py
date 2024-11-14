0ed3fec06b260892831374c8e9995a0d36b76e99
Hayato Tsumura <tp.tsumutsumu.1999@gmail.com>
2024-11-14 20:37:54 +0900

    datamodule の作成

--- src/data/datamodule.py
+++ src/data/datamodule.py
@@ -2,17 +2,29 @@ import lightning as L
 from torch.utils.data import random_split, DataLoader
 import pandas as pd
 import torch
-import transformers
+from transformers import AutoTokenizer, EsmModel
 from rdkit import Chem
 import re
 import os
+from tqdm import tqdm
 
 from src.utils.config import Config
 
+def normalize_smiles(smi, canonical, isomeric):
+    try:
+        normalized = Chem.MolToSmiles(
+            Chem.MolFromSmiles(smi), canonical=canonical, isomericSmiles=isomeric
+        )
+    except:
+        normalized = None
+    return normalized
+
 class PropertyPredictionDataset(torch.utils.data.Dataset):
-    def __init__(self, df, measure_name, tokenizer=None):
+    def __init__(self, df, measure_name=None, tokenizer=None, stage=None):
         df = df[['smiles', 'target_sequence', measure_name]]
         df = df.dropna()
+        # target_sequence に IDを付与する
+        df['target_id'] = df['target_sequence'].factorize()[0]
         self.measure_name = measure_name
         df['canonical_smiles'] = df['smiles'].apply(lambda smi: normalize_smiles(smi, canonical=True, isomeric=False))
         df['replaced_sequence'] = df['target_sequence'].apply(lambda seq: " ".join(list(re.sub(r"[UZOB]", "X", seq))))
@@ -25,22 +37,21 @@ class PropertyPredictionDataset(torch.utils.data.Dataset):
         self.df = df_good
         self.df = self.df.reset_index(drop=True)
         print("Length of dataset:", len(self.df))
-
+        
     def __getitem__(self, index):
-
         canonical_smiles = self.df.loc[index, 'canonical_smiles']
-        replaced_sequence = self.df.loc[index, 'replaced_sequence']
-        measures = self.df.loc[index, self.measure_name]
-        return canonical_smiles, replaced_sequence, measures
+        canonical_smiles = torch.tensor(canonical_smiles.values, dtype=torch.float32)
+        target_id = self.df.loc[index, 'target_id']
+        target_id = torch.tensor(target_id, dtype=torch.float32)
+        if self.stage=='predict':
+            return canonical_smiles, target_id
+        else:
+            measures = self.df.loc[index, self.measure_name]
+            measures = torch.tensor(measures, dtype=torch.float32)
+            return canonical_smiles, target_id, measures, index
   
     def __len__(self):
         return len(self.df)
-
-def get_dataset(dataset_path, measure_name):
-    df = pd.read_csv(dataset_path)
-    print("Length of dataset:", len(df))
-    dataset = PropertyPredictionDataset(df,  measure_name)
-    return dataset
 
 class DTIDataModule(L.LightningDataModule):
     def __init__(self, config: Config):
@@ -48,93 +59,115 @@ class DTIDataModule(L.LightningDataModule):
         self.config = config
         self.cache_dir = self.config.cache_dir
         os.makedirs(self.cache_dir, exist_ok=True)
-        self.protein_tokenizer = transformers.AutoTokenizer.from_pretrained('facebook/esm2_t36_3B_UR50D', trust_remote_code=True)
-        self.smi_tokenizer = transformers.AutoTokenizer.from_pretrained('ibm/MoLFormer-XL-both-10pct', trust_remote_code=True)
+        self.protein_embedding_cache_file = os.path.join(self.cache_dir, f"tokenized_protein_embeddings.pt")
+        self.protein_masks_cache_file = os.path.join(self.cache_dir, f"tokenized_protein_masks.pt")
+        self.stage = None
+    
+    def prepare_data(self):
+        # load tokenizer
+        self.protein_tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t36_3B_UR50D")
+        self.smi_tokenizer = AutoTokenizer.from_pretrained('ibm/MoLFormer-XL-both-10pct', trust_remote_code=True)
+        # load data
+        df = pd.read_csv(self.config.dataset_path)
+        # make dataset
+        self.dataset = PropertyPredictionDataset(df, measure_name=self.config.measure_name)
+        
+        # tokenize and save cache if not exists
 
-    def tokenize_and_cache_dataset(self, dataset, split):
-        cache_file = os.path.join(self.cache_dir, f"tokenized_{split}.pt")
+        # check if cache exists
+        if os.path.exists(self.protein_embedding_cache_file) and os.path.exists(self.protein_masks_cache_file):
+            print(f"Loading tokenized dataset from cache")
+            self.protein_emebbings = torch.load(self.protein_embeddings_cache_file)
+            self.protein_masks = torch.load(self.protein_masks_cache_file)
         
-        # キャッシュが存在するか確認
-        if os.path.exists(cache_file):
-            print(f"Loading tokenized {split} dataset from cache: {cache_file}")
-            tokenized_data = torch.load(cache_file)
-            return tokenized_data
-        
-        # データセット全体をトークナイズ
-        smiles, sequences, measures = zip(*[dataset[i] for i in range(len(dataset))])
-        smi_tokens = self.smi_tokenizer.batch_encode_plus(smiles, padding=True, add_special_tokens=True)
-        protein_tokens = self.protein_tokenizer.batch_encode_plus(sequences, add_special_tokens=True, padding="longest", return_tensors="pt")
-        
-        if self.hparams.task_type == "classification":
-            tokenized_data = (
-                torch.tensor(smi_tokens['input_ids']), 
-                torch.tensor(smi_tokens['attention_mask']),
-                protein_tokens['input_ids'],
-                protein_tokens['attention_mask'], 
-                torch.tensor(measures, dtype=torch.long)
-            )
-        elif self.hparams.task_type == "regression":            
-            tokenized_data = (
-                torch.tensor(smi_tokens['input_ids']), 
-                torch.tensor(smi_tokens['attention_mask']),
-                protein_tokens['input_ids'],
-                protein_tokens['attention_mask'], 
-                torch.tensor(measures, dtype=torch.float)
-            )
+        # if not exists, tokenize and save cache
         else:
-            raise ValueError("Task type not recognized")
-        
-        # キャッシュに保存
-        torch.save(tokenized_data, cache_file)
-        print(f"Tokenized {split} dataset saved to cache: {cache_file}")
-        
-        return tokenized_data
+            protein_embeddings = {}
+            protein_masks = {}
+            model = EsmModel.from_pretrained("facebook/esm2_t36_3B_UR50D")
+            model.half().to("cuda")
+            for i in tqdm(range(len(df))):
+                target_id, target_sequence = df.loc[i, 'target_id'], df.loc[i, 'replaced_sequence']
+                protein_token = self.protein_tokenizer(target_sequence, max_length=2050, padding="max_length", return_tensors="pt").to("cuda")
+                output = model(**protein_token)
+                protein_embeddings[target_id] = output.last_hidden_state[0].detach().cpu()
+                protein_masks[target_id] = protein_token["attention_mask"][0].detach().cpu()
+            
+            self.protein_embeddings = protein_embeddings
+            self.protein_masks = protein_masks
+            torch.save(protein_embeddings, self.protein_embeddings_cache_file)
+            torch.save(protein_masks, self.protein_masks_cache_file)
     
     def setup(self, stage: str = None):
-        # TODO implement for dataset creation
-        self.dataset = get_dataset(self.config.data_root, self.config.dataset_path, self.config.measure_name)
-        dataset_size = len(self.dataset)
-        train_size = int(self.config.train_ratio * dataset_size)
-        val_size = int(self.config.val_ratio * dataset_size)
-        test_size = dataset_size - train_size - val_size
-
-        tokenized_dataset = self.tokenize_and_cache_dataset(self.dataset, "full")
-
-        # split dataset
-        self.train_dataset, self.val_dataset, self.test_dataset = random_split(tokenized_dataset, [train_size, val_size, test_size])
+        self.stage = stage
+        if stage == 'fit':
+            dataset_size = len(self.dataset)
+            train_size = int(self.config.train_ratio * dataset_size)
+            val_size = int(self.config.val_ratio * dataset_size)
+            test_size = dataset_size - train_size - val_size
+            # split dataset
+            self.train_dataset, self.val_dataset, self.test_dataset = random_split(self.dataset, [train_size, val_size, test_size])
+        elif stage == 'test':
+            self.test_dataset = self.dataset
+        elif stage == 'predict':
+            self.predict_dataset = self.dataset
+        else:
+            raise ValueError(f"Stage {stage} not recognized")
     
-    def collate(self, batch):
-        return batch
+    def collate_fn(self, batch):
+        if self.stage == 'predict':
+            smiles, target_ids, index = zip(*batch)
+            smi_tokens = self.smi_tokenizer(smiles, padding=True, add_special_tokens=True) # TODO: want to take out smiles from the tokenized cache (Calculation time may be almost the same...)
+            protein_embeddings = [self.protein_embeddings[i] for i in target_ids]  # batch_size * (2050, 2560)
+            protein_embeddings = torch.stack(protein_embeddings)  # (batch_size, 2050, 2560)
+            protein_masks = [self.protein_masks[i] for i in target_ids]
+            protein_masks = torch.stack(protein_masks)
+            return (torch.tensor(smi_tokens['input_ids']),
+                    torch.tensor(smi_tokens['attention_mask']),
+                    protein_embeddings,
+                    protein_masks,
+                    torch.tensor(index)
+                    )
+        else:
+            smiles, target_ids, measures, index = zip(*batch)
+            smi_tokens = self.smi_tokenizer(smiles, padding=True, add_special_tokens=True)
+            protein_embeddings = [self.protein_embeddings[i] for i in target_ids]  # batch_size * (2050, 2560)
+            protein_embeddings = torch.stack(protein_embeddings)  # (batch_size, 2050, 2560)
+            protein_masks = [self.protein_masks[i] for i in target_ids]
+            protein_masks = torch.stack(protein_masks)
+            return (torch.tensor(smi_tokens['input_ids']),
+                    torch.tensor(smi_tokens['attention_mask']),
+                    protein_embeddings,
+                    protein_masks,
+                    torch.tensor(measures, dtype=torch.long),
+                    torch.tensor(index)
+                    )
+
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
-                          batch_size=self.config.batch_size,
-                          num_workers=self.config.num_workers,
-                          shuffle=True)
+                        batch_size=self.config.batch_size,
+                        num_workers=self.config.num_workers,
+                        collate_fn=self.collate_fn,
+                        shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset,
                           batch_size=self.config.batch_size,
                           num_workers=self.config.num_workers,
+                          collate_fn=self.collate_fn,
                           shuffle=False)
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset,
                           batch_size=self.config.batch_size,
                           num_workers=self.config.num_workers,
+                          collate_fn=self.collate_fn,
                           shuffle=False)
 
     def predict_dataloader(self):
         return DataLoader(self.predict_dataset,
                           batch_size=self.config.batch_size,
                           num_workers=self.config.num_workers,
+                          collate_fn=self.collate_fn,
                           shuffle=False)
-
-def normalize_smiles(smi, canonical, isomeric):
-    try:
-        normalized = Chem.MolToSmiles(
-            Chem.MolFromSmiles(smi), canonical=canonical, isomericSmiles=isomeric
-        )
-    except:
-        normalized = None
-    return normalized
