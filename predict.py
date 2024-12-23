01f981cf0dcce4c97dc85dce905c2a93cdc835fc
Takuto-Koyama <takutoyo.0525@gmail.com>
2024-12-23 18:41:41 +0900

    大規模予測用のスクリプトを実装

--- predict.py
+++ predict.py
@@ -19,13 +19,8 @@ from chemglam.utils.config import Config
 import torch
 torch.set_float32_matmul_precision('medium')
 
-def main():
-    args = argparse.ArgumentParser()
-    args.add_argument("-c", "--config", type=str, default="./config/config_demo.json")
-    args = args.parse_args()
 
-    #json_fileからconfigを読み込む
-    config = Config(args.config)
+def predict(config):
     print(config)
     seed_everything(config.seed, workers=True)
     
@@ -57,7 +52,7 @@ def main():
                 
     def sigmoid(x):
         return 1 / (1 + torch.exp(-x))
-         
+        
     predictions = torch.cat(predictions, dim=0)
     
     if config.task_type == "classification" and not config.evidential:
@@ -84,6 +79,24 @@ def main():
     if config.target_columns is not None:
         df_pred[config.target_columns] = datamodule.dataset.df[config.target_columns].values
     df_pred.to_csv(f"./logs/{config.experiment_name}/prediction.csv", index=False)
+
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument("-c", "--config", type=str, default="./config/config_demo.json")
+    args.add_argument("-i", "--split_id", type=str, default=0)
+    args = args.parse_args()
+    
+    with open(f"local_data/large_cpi/split_{args.split_id}/protein_entry.txt", "r") as f:
+        uniprot_ids = f.read().splitlines()
+
+    for uniprot_id in uniprot_ids: 
+        config = Config(args.config)
+        config.experiment_name = f"{config.experiment_name}/split_{args.split_id}/{uniprot_id}"
+        config.cache_dir = f"{config.cache_dir}/split_{args.split_id}/{uniprot_id}"
+        config.dataset_csv_path = f"local_data/large_cpi/split_{args.split_id}/{uniprot_id}.csv"
+        predict(config)
+    
 
 if __name__ == "__main__":
     main()
